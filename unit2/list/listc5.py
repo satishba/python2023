4f05f78f20ca26c5346a3c5d662184656d6a46ca
satishba <satish.ashwath@gmail.com>
2023-06-13 16:52:49 +0530

    stack

--- unit2/list/listc5.py
+++ unit2/list/listc5.py
@@ -16,33 +16,49 @@ available.
 
 """
 
-stack=[]
-maxSize=10
-def insert():
-      print("insert")  
+size=5
+list1=[]
 
 def pop():
-    print("pop")  
+    if(len(list1)==0):
+        print("Empty Stack")
+        return
+    else:
+        print(f"{list1[len(list1)-1]} removed")
+        list1.remove(list1[len(list1)-1])
+        
+def push():
+    if len(list1)==5:
+        print("Stack Full")
+        return
+    else:
+        list1.append(input("Enter element to push on to stack: "))
+        
+def mult_pop(pop_size):
+    if(pop_size>len(list1)):
+        print(f"Not possible to pop {pop_size} elements")
+        return
+    else:
+        for i in range(pop_size):
+            list1.remove(list1[len(list1)-1])
+            
+def display():
+    print(list1)
+            
+def choice(i):
+    if(i==1):
+        push()
+    elif(i==2):
+        pop()
+    elif(i==3):
+        pop_size=int(input("Enter pop_size: "))
+        mult_pop(pop_size)
+    else:
+        display()
     
-    
-def multiplePop():
-    print("multiple pop")
-
-def displayStack():
-    print("display stack")
-    
-def printMenu(): 
-    print("menu")
-    
-def main():
-    printMenu()
-    
-if __name__ == main():
-    main()    
-    
-    
-    
-    
+while(True):
+   i=int(input("Enter your choice: "))
+   choice(i)
     
     
     
